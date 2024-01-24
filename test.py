60abacee780982ef93a1169a54b314115a48e933
nikshko <nikitasushko2021@gmail.com>
2024-01-24 12:57:23 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import seaborn as sns
+import numpy as np
 import matplotlib.pyplot as plt
 
 from sklearn.datasets import load_svmlight_file
@@ -11,10 +11,10 @@ from genetic_algorithm.ga_feature_selector import (
     CrossoverType,
     MutationType,
 )
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score, accuracy_score
 
 
-def plot_history(history, selection_type, crossover_type, mutation_type):
+def plot_history(history, selection_type, crossover_type, mutation_type, frame_size):
     scores, features_num = [], []
     for item in history:
         scores.append(item[2])
@@ -32,10 +32,12 @@ def plot_history(history, selection_type, crossover_type, mutation_type):
     axes[1].set_xlabel("Generation number")
     axes[1].set_ylabel("F1 Score")
     axes[1].legend(loc="upper right")
-    plt.savefig(f"./plots/{selection_type}_{crossover_type}_{mutation_type}.png")
+    plt.savefig(
+        f"./plots/{selection_type}_{crossover_type}_{mutation_type}_{frame_size}.png"
+    )
 
 
-NO_OF_FEATURES = 300
+NO_OF_FEATURES = 1000
 
 # Assuming 'farm-ads-vect' is in SVMlight format
 X, y = load_svmlight_file("data/farm-ads-vect")
@@ -57,35 +59,45 @@ X_train, X_val, y_train, y_val = train_test_split(
     X_train_all, y_train_all, test_size=0.2, random_state=42
 )
 
+
 ga = GAFeatureSelector(
     estimator=KNeighborsClassifier,
     fitness_func=f1_score,
-    n_jobs=-1,
     verbose=True,
     population_size=50,
-    n_generations=50,
+    n_generations=100,
     mutation_rate=0.1,
     crossover_rate=0.8,
-    random_state=42,
+    # random_state=42,
     selection_type=SelectionType.TOURNAMENT,
     crossover_type=CrossoverType.UNIFORM,
-    mutation_type=MutationType.MULTI_BIT,
+    mutation_type=MutationType.ONE_BIT,
 )
 
 best_chromo, best_score = ga.fit(X_train, y_train, X_val, y_val)
 print(f"Best chromosome: {best_chromo}")
 
-X_test, y_test = X_test.iloc[:, best_chromo], y_test
+X_temp_test, y_temp_test = X_test.iloc[:, best_chromo], y_test
 model = KNeighborsClassifier()
 model.fit(X_train.iloc[:, best_chromo], y_train)
-y_pred = model.predict(X_test)
-print(f"Test F1 score: {f1_score(y_test, y_pred)}")
+y_pred = model.predict(X_temp_test)
+test_score = f1_score(y_temp_test, y_pred)
+print(f"Test score: {test_score}")
+acc = accuracy_score(y_temp_test, y_pred)
+print(f"Accuracy: {acc}")
 
 history = ga.get_best_history()
+scores, features_num = [], []
+for item in history:
+    scores.append(item[2])
+    features_num.append(item[1])
 
+df = pd.DataFrame({"num_features": features_num, "f1_score": scores})
+df.to_csv("history.csv")
 plot_history(
     history,
     ga.selection_type.value,
     ga.crossover_type.value,
     ga.mutation_type.value,
+    1000,
 )
